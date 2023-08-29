7e1bcfc2dfdfd9a30e001199b7a42304c08777e4
Lost <lostforyou67a@gmail.com>
2023-08-29 15:10:45 -0500

    Version-1.0.6

--- src/UserFileManager.java
+++ src/UserFileManager.java
@@ -1,10 +1,20 @@
+import org.json.JSONObject;
+
 import java.io.*;
 public class UserFileManager {
-    private static final String FILE_PATH = "./userFiles/";
+    private static final String FILE_PATH = "src/userFiles/";
     public static void saveUser(User user) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH))){
-            oos.writeObject(user);
-            System.out.println("User saved to file");
+        try (FileWriter fileWriter = new FileWriter(FILE_PATH + user.getMyGMAIl() + ".json")){
+            JSONObject jsonUser = new JSONObject();
+
+            jsonUser.put("name", user.getMyNAME());
+            jsonUser.put("lastName", user.getMyLASTNAME());
+            jsonUser.put("age", user.getMyAGE());
+            jsonUser.put("gmail", user.getMyGMAIl());
+            jsonUser.put("password", user.getMyPASSWORD());
+
+            fileWriter.write(jsonUser.toString());
+            System.out.println("User saved to file: " + jsonUser.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
